44ac4d7713b58675eadf3fa4ec1b355695754d8b
Siponek <szinkowicz@gmail.com>
2022-12-07 15:13:49 +0100

    Fake SPID workaround

---
+++ pages/check_event_fake.php
@@ -0,0 +1,9 @@
+<?php
+$check_evento = 0;
+$contatore_eventi = 0;
+$contatore_allerte = 0;
+$check_pausa = 0;
+$descrizione_allerta = 'Nessuna allerta';
+$color_allerta = '#5cb85c';
+$profilo_sistema = 0;
+?>
