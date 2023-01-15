5c9d03c8f5eba593eb642260df10c5ce8f7e9bb8
Siponek <szinkowicz@gmail.com>
2023-01-15 15:34:09 +0100

    Bypass for SPID number

--- pages/check_event_fake.php
+++ pages/check_event_fake.php
@@ -3,7 +3,15 @@ $check_evento = 0;
 $contatore_eventi = 0;
 $contatore_allerte = 0;
 $check_pausa = 0;
-$descrizione_allerta = 'Nessuna allerta';
-$color_allerta = '#5cb85c';
+$descrizione_allerta = "Nessuna allerta";
+$color_allerta = "#5cb85c";
 $profilo_sistema = 0;
+// Weird variables
+$eventi_attivi_c[] = [0];
+$profilo_ok = 0;
+$segn_limbo = 0;
+$segn_limbo_municipi = 0;
+$count_resp = 0;
+$count_squadra = 0;
+$check_esterno_update = 0;
 ?>
